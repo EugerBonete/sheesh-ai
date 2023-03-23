061bf68741a4654f87c1f947843ea0115e4e2b0b
euger <azkriven17@gmail.com>
2023-03-23 17:35:24 +0800

    resized navbar logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="shadow-lg px-5 ">
-      <img src="./logo2.png" alt="" className="object-fit w-[15rem]" />
+      <img src="./logo2.png" alt="" className="object-fit w-[12rem] h-[5rem]" />
     </nav>
   );
 }
